f73fb4a5a932b3c1e57cca2e8136fd94287e3b2b
shyu850 <uttamchoubay1978@gmail.com>
2024-07-13 13:43:58 +0530

    Quiz App
    "Challenge your knowledge with our Quiz App! Featuring a variety of categories, timed quizzes, and instant feedback, it's the perfect way to learn and have fun. Compete with friends, track your progress, and discover new topics with our user-friendly interface."

---
+++ app/src/main/java/com/example/quiz/QuestionsAnswer.java
@@ -0,0 +1,30 @@
+package com.example.quiz;
+public class QuestionsAnswer {
+    public static String question[] = {
+            "Which Company Owns The Android?",
+            "Which one is not the Programming Language?",
+            "Where you are running this App?",
+            "Android is- ?",
+            "For which of the following Android is mainly developed?"
+
+    };
+
+    public static  String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Sololearn","Visual Studio","Online Compiler","Android Studio"},
+            {"an operating system" ,"a web browser" ,"a web server ","None of the above"},
+            {"Servers","Desktops","Laptops","Mobile devices"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Android Studio",
+            "an operating system",
+            "Mobile devices"
+
+    };
+
+}
